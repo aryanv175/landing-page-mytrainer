0f92efe95963d1d18e3420bd447a3124874c00ae
Aryan Verma <91381804+aryanv175@users.noreply.github.com>
2024-06-29 02:35:04 +0530

    trying

--- landing-page-mytrainer/src/App.js
+++ landing-page-mytrainer/src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js (continued)
+import React from 'react';
+import styled from 'styled-components';
+import AppInfo from './components/AppInfo';
+import mobileImage from './assets/mobile.png'; // Make sure to place your image in the src/assets folder
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  padding: 20px;
+`;
+
+const Header = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+  font-size: 24px;
+`;
+
+const Title = styled.h1`
+  font-size: 2.5em;
+`;
+
+const Subtitle = styled.p`
+  font-size: 1.2em;
+`;
+
+const DownloadButton = styled.button`
+  background-color: #007bff;
+  color: white;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 1em;
+`;
+
+const Image = styled.img`
+  width: 200px;
+  margin-top: 20px;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header>
+        <span role="img" aria-label="muscle">
+          💪
+        </span>
+        MyTrainer
+      </Header>
+      <Title>Your workout. Your way.</Title>
+      <Subtitle>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus in libero risus semper habitant arcu eget. Et integer facilisi eget.
+      </Subtitle>
+      <DownloadButton>Download the App</DownloadButton>
+      <Image src={mobileImage} alt="Mobile App" />
+      <AppInfo />
+    </Container>
   );
 }
 
---
+++ landing-page-mytrainer/src/components/AppInfo.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const InfoContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
+  margin-top: 20px;
+`;
+
+const InfoBlock = styled.div`
+  background: black;
+  color: white;
+  padding: 20px;
+  border-radius: 5px;
+  text-align: center;
+`;
+
+const InfoNumber = styled.div`
+  font-size: 2em;
+`;
+
+const InfoText = styled.div`
+  font-size: 0.8em;
+`;
+
+function AppInfo() {
+  return (
+    <InfoContainer>
+      <InfoBlock>
+        <InfoNumber>4.9</InfoNumber>
+        <InfoText>Lorem ipsum dolor amet</InfoText>
+      </InfoBlock>
+      <InfoBlock>
+        <InfoNumber>876K</InfoNumber>
+        <InfoText>Lorem ipsum dolor amet</InfoText>
+      </InfoBlock>
+      <InfoBlock>
+        <InfoNumber>No.1</InfoNumber>
+        <InfoText>Lorem ipsum dolor amet</InfoText>
+      </InfoBlock>
+    </InfoContainer>
+  );
+}
+
+export default AppInfo;
