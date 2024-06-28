4e5a585687a293aa20ae5b2907243928fa8be09e
Aryan Verma <91381804+aryanv175@users.noreply.github.com>
2024-06-29 03:35:42 +0530

    wow

--- landing-page-mytrainer/src/App.js
+++ landing-page-mytrainer/src/App.js
@@ -2,41 +2,57 @@
 import React from 'react';
 import styled from 'styled-components';
 import AppInfo from './components/AppInfo';
-import mobileImage from './assets/mobile.png'; // Make sure to place your image in the src/assets folder
+import mobileImage from './assets/mobile.png'; // Ensure your image path is correct
 import emojiImage from './assets/emoji.png';
+import starImage from './assets/star.png'; // Import the star image
 
 const Container = styled.div`
-  font-family: poppins;
+  font-family: Poppins, sans-serif;
   display: flex;
   flex-direction: column;
   align-items: center;
   text-align: center;
-  padding: 6px 24px;
+  padding: 10px 24px;
+`;
+
+const TitleContainer = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
 `;
 
 const StyledImage = styled.img`
-  width: 64px;  // Set your desired width
-  height: 64px; // Adjust height automatically to maintain aspect ratio
+  position: relative;
+  top: -5px;
+  width: 64px;
+  height: 64px;
   margin-right: 12px;
   margin-top: 8px;
 `;
 
 const StyledImage2 = styled.img`
-  width: 475px;  // Set your desired width
+  width: 475px;
   margin-right: 12px;
   margin-top: 8px;
 `;
 
+const StarImage = styled.img`
+  position: absolute;
+  top: 11px; // Adjust the position as needed
+  left: 283px; // Adjust the position as needed
+  width: 20px; // Adjust the size as needed
+  height: 20px; // Adjust the size as needed
+`;
+
 const Title = styled.h1`
   font-size: 64px;
-  font-family: poppins;
   margin-top: 0px;
   margin-bottom: 0px;
 `;
 
 const Tagline = styled.h1`
   margin-top: 0px;
-  margin-bottom: 0px;
+  margin-bottom: -8px;
   font-size: 64px;
   text-align: left;
 `;
@@ -61,21 +77,17 @@ const DownloadButton = styled.button`
   font-weight: 700;
 `;
 
-
-
 const Div2 = styled.div`
   display: flex;
   justify-content: space-around;
   align-items: center;
   width: 100%;
   padding: 20px;
-
 `;
 
 const Div3 = styled.div`
   text-align: left;
 `;
-
 
 const HalfWidthDiv = styled.div`
   flex: 1;
@@ -85,10 +97,11 @@ const HalfWidthDiv = styled.div`
 function App() {
   return (
     <Container>
-      <Title>
+      <TitleContainer>
         <StyledImage src={emojiImage} alt="Mobile App" />
-        MyTrainer
-      </Title>
+        <Title>MyTrainer</Title>
+        <StarImage src={starImage} alt="Star Image" />
+      </TitleContainer>
       <Div2>
         <HalfWidthDiv>
           <Tagline>Your workout.</Tagline>
