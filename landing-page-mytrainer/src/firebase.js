f312fd8f8e9b3d68f4aabc60305485d1e1386fa6
Aryan Verma <91381804+aryanv175@users.noreply.github.com>
2024-06-30 01:51:59 +0530

    debugging

--- landing-page-mytrainer/src/firebase.js
+++ landing-page-mytrainer/src/firebase.js
@@ -3,13 +3,7 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, addDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDXlwsVtraENHz15odd6MgzPflYeJzi7bc",
-    authDomain: "mytrainer-decab.firebaseapp.com",
-    projectId: "mytrainer-decab",
-    storageBucket: "mytrainer-decab.appspot.com",
-    messagingSenderId: "44874881991",
-    appId: "1:44874881991:web:59a219aeb85a1c84cfd7e2",
-    measurementId: "G-G83KN3N9R5"
+
 };
 
 const app = initializeApp(firebaseConfig);
