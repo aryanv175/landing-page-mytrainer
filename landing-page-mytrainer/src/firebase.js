9b507c0ee2db5db7607ac1433e501dd3ae731ffd
Aryan Verma <91381804+aryanv175@users.noreply.github.com>
2024-06-30 02:23:29 +0530

    great

--- landing-page-mytrainer/src/firebase.js
+++ landing-page-mytrainer/src/firebase.js
@@ -1,6 +1,8 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, addDoc } from 'firebase/firestore';
 
+
+
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
